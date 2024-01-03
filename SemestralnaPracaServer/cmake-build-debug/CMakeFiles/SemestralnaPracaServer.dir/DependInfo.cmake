049d27ef60fb5e40e9950060b0960a89fcf8d5a7
fabmich <146347212+fabmich@users.noreply.github.com>
2024-01-03 18:27:59 +0100

    upload serverovej casti
    proof of concept - zaklad serverovej casti a vyriesena komunikacia so server - client

---
+++ SemestralnaPracaServer/cmake-build-debug/CMakeFiles/SemestralnaPracaServer.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/tmp/tmp.bj8FyVHl8L/main.c" "/tmp/tmp.bj8FyVHl8L/cmake-build-debug/CMakeFiles/SemestralnaPracaServer.dir/main.c.o"
+  "/tmp/tmp.bj8FyVHl8L/pos_sockets/active_socket.c" "/tmp/tmp.bj8FyVHl8L/cmake-build-debug/CMakeFiles/SemestralnaPracaServer.dir/pos_sockets/active_socket.c.o"
+  "/tmp/tmp.bj8FyVHl8L/pos_sockets/char_buffer.c" "/tmp/tmp.bj8FyVHl8L/cmake-build-debug/CMakeFiles/SemestralnaPracaServer.dir/pos_sockets/char_buffer.c.o"
+  "/tmp/tmp.bj8FyVHl8L/pos_sockets/passive_socket.c" "/tmp/tmp.bj8FyVHl8L/cmake-build-debug/CMakeFiles/SemestralnaPracaServer.dir/pos_sockets/passive_socket.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
